babdd76c13d7151b20eadcaeb86da8e25f2c637a
Byunghun Jeon <bjeon@codeaurora.org>
2014-12-05 18:44:11 -0800

    WindowManager: Upper level changes to expose blur effect
    Upper level changes to expose blur-behind and blur-mask effect
    
    Change-Id: I6d37b43888c8c5e028974bd714596d8178cb5114
    
    WindowManager: Adding template for BlurLayer file
    
    Cyanogen changes:
      * Use config.xml instead of settings
      * Don't break stuff if not supported in SF
      * Incremental fade for insecure lockscreen
      * Disabled extra effects for now
    
    Change-Id: Icefe452f7a015656661e8543849c9b88889dbb40
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2486,6 +2486,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mScreenOnComingFromTouch;
     }
 
+    void setBlur(float b){
+        mStatusBarWindowManager.setBlur(b);
+    }
+
     public boolean isFalsingThresholdNeeded() {
         boolean onKeyguard = getBarState() == StatusBarState.KEYGUARD;
         //boolean isMethodInsecure = mUnlockMethodCache.isMethodInsecure();
@@ -3459,7 +3463,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void addStatusBarWindow() {
         makeStatusBarView();
-        mStatusBarWindowManager = new StatusBarWindowManager(mContext);
+        mStatusBarWindowManager = new StatusBarWindowManager(mContext, this);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
     }
 
@@ -4115,6 +4119,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mHandler.removeMessages(MSG_LAUNCH_TRANSITION_TIMEOUT);
         return staying;
+    }
+
+    boolean isSecure() {
+        return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isSecure();
     }
 
     public long calculateGoingToFullShadeDelay() {
