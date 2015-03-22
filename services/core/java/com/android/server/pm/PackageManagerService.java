d105992234815a01918046bb2f31f1a16b7e78dd
Steve Kondik <steve@cyngn.com>
2015-03-22 12:23:35 -0700

    packagemanager: Use ArrayMap/ArraySet as per AOSP
     * To reduce memory consumption
    
    Change-Id: I85bdf84d4a507532e41fe29668fef9b4eabfb04a
    
    Conflicts:
    	services/core/java/com/android/server/pm/PackageManagerService.java

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -412,7 +412,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     final int[] mGlobalGids;
     final SparseArray<ArraySet<String>> mSystemPermissions;
     final ArrayMap<String, FeatureInfo> mAvailableFeatures;
-    final ArrayMap<Signature, HashSet<String>> mSignatureAllowances;
+    final ArrayMap<Signature, ArraySet<String>> mSignatureAllowances;
 
     // If mac_permissions.xml was found for seinfo labeling.
     boolean mFoundPolicyFile;
@@ -498,7 +498,20 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     boolean mResolverReplaced = false;
 
+<<<<<<< HEAD
     private AppOpsManager mAppOps;
+=======
+    private Map<String, Long> mAvailableCommonResources = new ArrayMap<String, Long>();
+
+    private ThemeConfig mBootThemeConfig;
+
+    final ResolveInfo mPreLaunchCheckResolveInfo = new ResolveInfo();
+    ComponentName mCustomPreLaunchComponentName;
+    private Set<String> mPreLaunchCheckPackages =
+            Collections.synchronizedSet(new ArraySet<String>());
+
+    boolean mPreLaunchCheckPackagesReplaced = false;
+>>>>>>> 3e6ec90... packagemanager: Use ArrayMap/ArraySet as per AOSP
 
     ArrayList<ComponentName> mDisabledComponentsList;
 
@@ -2717,7 +2730,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     private boolean isAllowedSignature(PackageParser.Package pkg, String permissionName) {
         for (Signature pkgSig : pkg.mSignatures) {
-            HashSet<String> perms = mSignatureAllowances.get(pkgSig);
+            ArraySet<String> perms = mSignatureAllowances.get(pkgSig);
             if (perms != null && perms.contains(permissionName)) {
                 return true;
             }
@@ -2899,7 +2912,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             return PackageManager.SIGNATURE_NO_MATCH;
         }
 
-        // Since both signature sets are of size 1, we can compare without HashSets.
+        // Since both signature sets are of size 1, we can compare without ArraySets.
         if (s1.length == 1) {
             return s1[0].equals(s2[0]) ?
                     PackageManager.SIGNATURE_MATCH :
