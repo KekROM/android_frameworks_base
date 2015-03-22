d161fba5d2eff33d51cbe3dcaf9d0680dd433ff0
Steve Kondik <steve@cyngn.com>
2015-03-22 14:48:57 -0700

    systemui: Fix compilation issues
     * Update tiles for new APIs
     * Fix merge conflicts
     * Animation code in the screen timeout tile needs refactored- it is
       broken as-is in CM12.
    
    Change-Id: I97f2104bf268cd398688235f72f5a623392aca13
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3971,6 +3971,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mWindowManager.removeViewImmediate(mNavigationBarView);
             mNavigationBarView = null;
         }
+        if (mHandlerThread != null) {
+            mHandlerThread.quitSafely();
+            mHandlerThread = null;
+        }
         mContext.unregisterReceiver(mBroadcastReceiver);
     }
 
